55695317c2254f35fce256b2fdf8b18153b885f6
John <ashton.basile@gmail.com>
2023-08-09 13:20:10 +0100

    I wrote a test for the findMostPopularDinosaur function and the findAllDinosaursOfSpecies function.

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -5,11 +5,13 @@ const Dinosaur = require('../models/dinosaur.js');
 describe('Park', function() {
 	let dinosaur1;
 	let dinosaur2;
+	let dinosaur3;
 	let park1;
 
 	this.beforeEach(function () {
 		dinosaur1 = new Dinosaur('t-rex', 'carnivore', 50);
 		dinosaur2 = new Dinosaur('triceratops', "herbivore", 20);
+		dinosaur3 = new Dinosaur('t-rex', 'carnivore', 10);
 		park1 = new Park('Isla Nublar', 300, [dinosaur1]);
 	});
 
@@ -41,9 +43,17 @@ describe('Park', function() {
 	});
 
 	it('should be able to find the dinosaur that attracts the most visitors', function () {
+		park1.addDinosaurToCollection(dinosaur2)
+		const actual = park1.findMostPopularDinosaur();
+		assert.deepStrictEqual(actual, dinosaur1);
 	});
 
-	it('should be able to find all dinosaurs of a particular species');
+	it('should be able to find all dinosaurs of a particular species', function () {
+		park1.addDinosaurToCollection(dinosaur2);
+		park1.addDinosaurToCollection(dinosaur3);
+		const actual = park1.findAllDinosaursOfSpecies('t-rex');
+		assert.deepStrictEqual(actual, [dinosaur1, dinosaur3]);
+	});
 
 	it('should be able to calculate the total number of visitors per day');
 
